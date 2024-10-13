d437b26e17d8be6d4962ffeac7afd8748ace62ec
CarolinaCedro <yotechgirl@gmail.com>
2024-10-13 00:53:04 -0300

    ++

--- src/main/java/tcc/sgmeabackend/SgmeaBackendApplication.java
+++ src/main/java/tcc/sgmeabackend/SgmeaBackendApplication.java
@@ -4,6 +4,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 
@@ -13,7 +14,7 @@ public class SgmeaBackendApplication {
 
     protected static final Logger logger = LogManager.getLogger();
 
-
+    @Profile({"dev","prod"})
     public static void main(String[] args) {
         logger.info("Hello World!");
 
