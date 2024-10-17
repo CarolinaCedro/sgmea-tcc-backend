702fa623c692a977f633b578493b44d1d337fdd2
Carolina Cedro <yotechgirl@gmail.com>
2024-10-16 21:51:23 -0300

    ++

--- src/main/java/tcc/sgmeabackend/infra/security/SecurityConfigurations.java
+++ src/main/java/tcc/sgmeabackend/infra/security/SecurityConfigurations.java
@@ -69,7 +69,7 @@ public class SecurityConfigurations {
         configuration.setAllowedOrigins(Arrays.asList(
                 "http://localhost:4200",
                 "http://ec2-3-208-47-127.compute-1.amazonaws.com:8083",
-                "https://sgmea-tcc-frontend-px0vxecpp-carolinacedros-projects.vercel.app"
+                "http://sgmea-tcc-frontend-px0vxecpp-carolinacedros-projects.vercel.app"
         )); // Adicionando a URL da Vercel
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE")); // Métodos permitidos
         configuration.setAllowedHeaders(Arrays.asList("*")); // Cabeçalhos permitidos
