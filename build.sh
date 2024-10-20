a77f24d82af5aa9662ac254885355816527b1064
Carolina Cedro <yotechgirl@gmail.com>
2024-10-20 11:32:51 -0300

    add pipe for profile change

--- build.sh
+++ build.sh
@@ -1,13 +1,16 @@
+#!/bin/bash
+
+# Passo 1: Faz o pull da branch master
 echo "Fazendo git pull na branch master..."
 git pull origin master
 
-# Passo 3: Compila o projeto (certifique-se de ter o Maven ou Gradle instalado)
+# Passo 2: Compila o projeto (certifique-se de ter o Maven ou Gradle instalado)
 echo "Compilando o projeto..."
 mvn clean install  # ou `./gradlew build` se estiver usando Gradle
 
-# Passo 4: Executa a aplicação Spring Boot com limite de memória
-echo "Iniciando a aplicação com limite de memória..."
-java -Xms256m -Xmx512m -jar target/sgmea-backend-0.0.1-SNAPSHOT.jar
+# Passo 3: Executa a aplicação Spring Boot com limite de memória e profile prod
+echo "Iniciando a aplicação com limite de memória e profile 'prod'..."
+sudo java -Xms256m -Xmx512m -jar target/sgmea-backend-0.0.1-SNAPSHOT.jar --spring.profiles.active=prod
 
 # Fim do script
-echo "Aplicação iniciada com sucesso!"
+echo "Aplicação iniciada com sucesso no profile 'prod'!"
